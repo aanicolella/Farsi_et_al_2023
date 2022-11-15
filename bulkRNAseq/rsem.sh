48196c5106551c18611472ad07b60f877599d213
Ally Nicolella <31012168+aanicolella@users.noreply.github.com>
2022-11-15 10:24:04 -0500

    Update rsem.sh

--- bulkRNAseq/rsem.sh
+++ bulkRNAseq/rsem.sh
@@ -1,1 +1,11 @@
-rsem-calculate-expression -p 8  --bowtie2 --paired-end --estimate-rspd --append-names --sort-bam-by-coordinate parentDir/sample/sample.unmapped.1.fastq.gz parentDir/sample/sample.unmapped.2.fastq.gz homeDir/references/mouse_gencode/mouse_gencode parentDir/sample/sample
+#!/bin/bash -l
+
+INPUTSAMPLES=(x y z)
+SAMPLENAME="${INPUTSAMPLES[$SGE_TASK_ID - 1]}"
+
+#$ -cwd
+#$ -l h_vmem=100g
+#$ -l h_rt=48:00:00
+#$ -t 1-numReps
+
+rsem-calculate-expression -p 8  --bowtie2 --paired-end --estimate-rspd --append-names --sort-bam-by-coordinate /path/to/data/$SAMPLENAME/$SAMPLENAME.unmapped.1.fastq.gz /path/to/data/$SAMPLENAME/$SAMPLENAME.unmapped.2.fastq.gz /path/to/reference /path/to/data/$SAMPLENAME/$SAMPLENAME
